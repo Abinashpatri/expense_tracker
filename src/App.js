9dc071e73b9da0ad9bf3842e6083c2514a33c92a
Abinash Patri <97664278+Abinashpatri@users.noreply.github.com>
2024-12-20 13:45:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import NewTranjactions from "./components/NewTranjactions";
 import Tranjactions from "./components/Tranjactions";
 
 const Header = styled(Typography)`
-  font-size: 38px;
+  font-size: 32px;
   color: white;
   text-transform: uppercase;
 `;
